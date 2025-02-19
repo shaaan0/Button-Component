0eb6eea669f74fc71a2408203a649bf70127697b
Sudipta Singh <ss.dps47@gmail.com>
2025-02-19 19:26:58 +0530

    added different props to buttons

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ const App = () => {
     <div className="main">
       <div className="container">
         <div>
-          <Button type={"primary"} size={"m"} leftStar={true} rightStar={true} isDisabled={true}>
+          <Button type={"primary"} size={"m"} leftStar={false} rightStar={false} isDisabled={false}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"primary"} size={"l"} leftStar={true} rightStar={true}>
+        <Button type={"primary"} size={"l"} leftStar={true} rightStar={false}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"primary"} size={"xl"} leftStar={true} rightStar={true}>
+        <Button type={"primary"} size={"xl"} leftStar={false} rightStar={true}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"primary"} size={"xxl"} leftStar={true} rightStar={true}>
+        <Button type={"primary"} size={"xxl"} leftStar={false} rightStar={false}>
             Button CTA
           </Button>
         </div>
@@ -35,22 +35,22 @@ const App = () => {
       </div>
       <div className="container">
         <div>
-          <Button type={"secondary"} size={"m"} leftStar={true} rightStar={true} isDisabled={true}>
+          <Button type={"secondary"} size={"m"} leftStar={false} rightStar={false} isDisabled={false}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"secondary"} size={"l"} leftStar={true} rightStar={true}>
+        <Button type={"secondary"} size={"l"} leftStar={true} rightStar={false}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"secondary"} size={"xl"} leftStar={true} rightStar={true}>
+        <Button type={"secondary"} size={"xl"} leftStar={false} rightStar={true}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"secondary"} size={"xxl"} leftStar={true} rightStar={true}>
+        <Button type={"secondary"} size={"xxl"} leftStar={false} rightStar={false}>
             Button CTA
           </Button>
         </div>
@@ -62,22 +62,22 @@ const App = () => {
       </div>
       <div className="container">
         <div>
-          <Button type={"tertiary"} size={"m"} leftStar={true} rightStar={true} isDisabled={true}>
+          <Button type={"tertiary"} size={"m"} leftStar={false} rightStar={false} isDisabled={false}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"tertiary"} size={"l"} leftStar={true} rightStar={true}>
+        <Button type={"tertiary"} size={"l"} leftStar={true} rightStar={false}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"tertiary"} size={"xl"} leftStar={true} rightStar={true}>
+        <Button type={"tertiary"} size={"xl"} leftStar={false} rightStar={true}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"tertiary"} size={"xxl"} leftStar={true} rightStar={true}>
+        <Button type={"tertiary"} size={"xxl"} leftStar={false} rightStar={false}>
             Button CTA
           </Button>
         </div>
@@ -89,22 +89,22 @@ const App = () => {
       </div>
       <div className="container">
         <div>
-          <Button type={"link"} size={"m"} leftStar={true} rightStar={true} isDisabled={true}>
+          <Button type={"link"} size={"m"} leftStar={false} rightStar={false} isDisabled={false}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"link"} size={"l"} leftStar={true} rightStar={true}>
+        <Button type={"link"} size={"l"} leftStar={true} rightStar={false}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"link"} size={"xl"} leftStar={true} rightStar={true}>
+        <Button type={"link"} size={"xl"} leftStar={false} rightStar={true}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"link"} size={"xxl"} leftStar={true} rightStar={true}>
+        <Button type={"link"} size={"xxl"} leftStar={false} rightStar={false}>
             Button CTA
           </Button>
         </div>
@@ -116,22 +116,22 @@ const App = () => {
       </div>
       <div className="container">
         <div>
-          <Button type={"link"} color={"gray"} size={"m"} leftStar={true} rightStar={true} isDisabled={true}>
+          <Button type={"link"} color={"gray"} size={"m"} leftStar={false} rightStar={false} isDisabled={false}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"link"} color={"gray"} size={"l"} leftStar={true} rightStar={true}>
+        <Button type={"link"} color={"gray"} size={"l"} leftStar={true} rightStar={false}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"link"} color={"gray"} size={"xl"} leftStar={true} rightStar={true}>
+        <Button type={"link"} color={"gray"} size={"xl"} leftStar={false} rightStar={true}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"link"} color={"gray"} size={"xxl"} leftStar={true} rightStar={true}>
+        <Button type={"link"} color={"gray"} size={"xxl"} leftStar={false} rightStar={false}>
             Button CTA
           </Button>
         </div>
@@ -143,22 +143,22 @@ const App = () => {
       </div>
       <div className="container">
         <div>
-          <Button type={"destructive"} size={"m"} leftStar={true} rightStar={true} isDisabled={true}>
+          <Button type={"destructive"} size={"m"} leftStar={false} rightStar={false} isDisabled={false}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"destructive"} size={"l"} leftStar={true} rightStar={true}>
+        <Button type={"destructive"} size={"l"} leftStar={true} rightStar={false}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"destructive"} size={"xl"} leftStar={true} rightStar={true}>
+        <Button type={"destructive"} size={"xl"} leftStar={false} rightStar={true}>
             Button CTA
           </Button>
         </div>
         <div>
-        <Button type={"destructive"} size={"xxl"} leftStar={true} rightStar={true}>
+        <Button type={"destructive"} size={"xxl"} leftStar={false} rightStar={false}>
             Button CTA
           </Button>
         </div>
