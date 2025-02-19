99ebc9da1b56c0bab93a2238dd7eef3f806ad3f9
Sudipta Singh <ss.dps47@gmail.com>
2025-02-19 19:19:16 +0530

    various buttons added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,175 @@
 import React from "react";
 import Button from "./Component/Button";
-import './css/style.css';
+import "./css/style.css";
+import StarIcon from "./Component/StarIcon";
 
 const App = () => {
-    return(
-        <div className="container">
-        <Button color={"primary"} size={"6xl"}>Button</Button>
+  return (
+    <div className="main">
+      <div className="container">
+        <div>
+          <Button type={"primary"} size={"m"} leftStar={true} rightStar={true} isDisabled={true}>
+            Button CTA
+          </Button>
         </div>
-    )
-}
+        <div>
+        <Button type={"primary"} size={"l"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"primary"} size={"xl"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"primary"} size={"xxl"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"primary"} size={"xxl"} leftStar={true} rightStar={false}>
+            
+          </Button>
+        </div>
+      </div>
+      <div className="container">
+        <div>
+          <Button type={"secondary"} size={"m"} leftStar={true} rightStar={true} isDisabled={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"secondary"} size={"l"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"secondary"} size={"xl"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"secondary"} size={"xxl"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"secondary"} size={"xxl"} leftStar={true} rightStar={false}>
+            
+          </Button>
+        </div>
+      </div>
+      <div className="container">
+        <div>
+          <Button type={"tertiary"} size={"m"} leftStar={true} rightStar={true} isDisabled={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"tertiary"} size={"l"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"tertiary"} size={"xl"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"tertiary"} size={"xxl"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"tertiary"} size={"xxl"} leftStar={true} rightStar={false}>
+            
+          </Button>
+        </div>
+      </div>
+      <div className="container">
+        <div>
+          <Button type={"link"} size={"m"} leftStar={true} rightStar={true} isDisabled={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"link"} size={"l"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"link"} size={"xl"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"link"} size={"xxl"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"link"} size={"xxl"} leftStar={true} rightStar={false}>
+            
+          </Button>
+        </div>
+      </div>
+      <div className="container">
+        <div>
+          <Button type={"link"} color={"gray"} size={"m"} leftStar={true} rightStar={true} isDisabled={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"link"} color={"gray"} size={"l"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"link"} color={"gray"} size={"xl"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"link"} color={"gray"} size={"xxl"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"link"} color={"gray"} size={"xxl"} leftStar={true} rightStar={false}>
+            
+          </Button>
+        </div>
+      </div>
+      <div className="container">
+        <div>
+          <Button type={"destructive"} size={"m"} leftStar={true} rightStar={true} isDisabled={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"destructive"} size={"l"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"destructive"} size={"xl"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"destructive"} size={"xxl"} leftStar={true} rightStar={true}>
+            Button CTA
+          </Button>
+        </div>
+        <div>
+        <Button type={"destructive"} size={"xxl"} leftStar={true} rightStar={false}>
+            
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/Component/Button.js
+++ src/Component/Button.js
@@ -1,13 +1,47 @@
-import React from 'react'
-import '../css/style.css'
+import React, { useEffect, useState } from "react";
+import "../css/style.css";
+import StarIcon from "./StarIcon";
 
-function Button({color, size, children}) {
+function Button({
+  type,
+  size,
+  color,
+  leftStar,
+  rightStar,
+  isDisabled,
+  children,
+}) {
+  const [spanClassName, setSpanClassName] = useState("");
+
+  const handleSpanClass = () => {
+    if (!isDisabled) {
+      setSpanClassName(`label-focus`);
+    }
+  };
+
   return (
-    <>
-    <button className={`button button-${color} button-${size}`}>{children}</button>
-    </>
-
-  )
+    <button
+      type="link"
+      className={`button button-${
+        type !== "link" ? type : ""
+      } button-${size}  ${isDisabled ? `button-${type}-disabled` : ""}`}
+    >
+      {type === "link" ? (
+        <span className={`label ${color ? 'gray':""} ${spanClassName} ${isDisabled? "label-disabled":""}` } onClick={handleSpanClass}
+        >
+          {leftStar ? <StarIcon size={size} /> : ""}
+          {children}
+          {rightStar ? <StarIcon size={size} /> : ""}
+        </span>
+      ) : (
+        <>
+          {leftStar ? <StarIcon size={size} /> : ""}
+          {children}
+          {rightStar ? <StarIcon size={size} /> : ""}
+        </>
+      )}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
